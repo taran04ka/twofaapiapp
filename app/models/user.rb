8b12674eed1d1161a14f3978a6c41975d85a2bfe
Taras Zhyhulin <zhyhulin@student.agh.edu.pl>
2024-05-24 15:29:41 +0200

    app configuration performed

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  devise :two_factor_authenticatable
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :registerable,
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: self
+end
